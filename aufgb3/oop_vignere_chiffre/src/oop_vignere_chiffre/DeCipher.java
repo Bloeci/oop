c884151dbf2f7d62a637a1e36eee014fcea8d4f0
Bloeci <m.beining@outlook.de>
2022-05-23 18:49:46 +0200

    new class to generate cipher table, just some methods to create table etc.

--- aufgb3/oop_vignere_chiffre/src/oop_vignere_chiffre/DeCipher.java
+++ aufgb3/oop_vignere_chiffre/src/oop_vignere_chiffre/DeCipher.java
@@ -13,35 +13,8 @@ public class DeCipher {
      * @param keyword   Used keyword to generate table.
      */
     public DeCipher (String keyword) {
-        this.cipherTable = generateCipherTable(keyword);
+        this.cipherTable = new CipherTable().generateCipherTable(keyword);
         this.keyword = keyword;
-    }
-
-    /**
-     * Generate a cipher table based on a given keyword length n.
-     * @param keyword   String to decipher a given text.
-     * @return cipher  Lookup table with size n.
-     */
-    private ArrayList<String> generateCipherTable(String keyword) {
-        ArrayList<String> cipher = new ArrayList<>(keyword.length());
-
-        int alphabetLetterIndex, cipherIndex;
-        char keywordLetter;
-        for (int i = 0; i < keyword.length(); i++) {
-            // get actual letter in keyword and index in alphabet
-            keywordLetter = keyword.charAt(i);
-            alphabetLetterIndex = this.alphabet.indexOf(keywordLetter);
-
-            StringBuilder cipherLine = new StringBuilder();
-            // iterate throw alphabet with modulo index
-            for (int j = 0; j < 26; j++) {
-                cipherIndex = (alphabetLetterIndex + j) % 26;
-                cipherLine.append(this.alphabet.charAt(cipherIndex));
-            }
-
-            cipher.add(cipherLine.toString());
-        }
-        return cipher;
     }
 
     /**

9b8cc8fdad5417956c9fd9963ff1580b2b4967c5
Bloeci <m.beining@outlook.de>
2022-06-17 23:52:16 +0200

    neu

---
+++ aufgb4/aquarium/src/Main.java
@@ -0,0 +1,39 @@
+import oop_aquarium.Fish;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(java.lang.String[] args) {
+
+    }
+
+    private ArrayList<Fish> generateFishes() {
+        String grueneMigraene = "Gruene Migraene";
+        String korallenqualle = "Korallenqualle";
+        String schuppenschatulle = "Schuppenschatulle";
+        String breitmaulfrosch = "Breitmaulfrosch";
+        String prachtpiranha = "Prachtpiranha";
+        String zitterling = "Zitterling";
+        String grottensprotte = "Grottensprotte";
+
+        // Generate some fishes
+        Fish fish1 = new Fish(grueneMigraene, 70, new String[]{breitmaulfrosch, grottensprotte});
+        Fish fish2 = new Fish(korallenqualle, 50, new String[]{});
+        Fish fish3 = new Fish(schuppenschatulle, 30, new String[]{breitmaulfrosch, prachtpiranha});
+        Fish fish4 = new Fish(breitmaulfrosch, 40, new String[]{grueneMigraene, schuppenschatulle});
+        Fish fish5 = new Fish(prachtpiranha, 40, new String[]{schuppenschatulle, grottensprotte});
+        Fish fish6 = new Fish(zitterling, 30, new String[]{grottensprotte});
+        Fish fish7 = new Fish(grottensprotte, 20, new String[]{grueneMigraene, prachtpiranha, zitterling});
+
+        // Add fishes to list
+        ArrayList<Fish> fishes = new ArrayList<Fish>();
+        fishes.add(fish1);
+        fishes.add(fish2);
+        fishes.add(fish3);
+        fishes.add(fish4);
+        fishes.add(fish5);
+        fishes.add(fish6);
+        fishes.add(fish7);
+        return fishes;
+    }
+}

2f93c4febd7875e926a11997077c5d542971652d
Bloeci <m.beining@outlook.de>
2022-06-26 17:44:58 +0200

    complete implementation

--- aufgb4/aquarium/src/oop_aquarium/Aquarium.java
+++ aufgb4/aquarium/src/oop_aquarium/Aquarium.java
@@ -1,68 +1,102 @@
 package oop_aquarium;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
+/**
+ * Class to generate a shopping list of fishes based on tolerances.
+ */
 public class Aquarium {
     private ArrayList<Fish> fishes;
-    private ArrayList<ArrayList<Fish>> recursiveFishList;
 
     /**
-     * Contructor of the aquarium class. Takes a list of fishes
-     * as input to generate an maximum list of fishes.
+     * Constructor of the aquarium class. Takes a list of fishes
+     * as input to generate a maximum list of fishes.
      * @param fishes    List of fishes.
      */
     public Aquarium(ArrayList<Fish> fishes){
         this.fishes = fishes;
-        this.recursiveFishList = new ArrayList<>();
     }
 
-    public ArrayList<ArrayList<Fish>> getRecursiveFishList(){
-        return this.recursiveFishList;
+    /**
+     * Generate optimal lists for a given price, which are tolerated each other,
+     * @param price Maximum money to buy fishes.
+     */
+    public void generateShoppingList(int price){
+        ArrayList<ArrayList<Fish>> result = new ArrayList<>();
+
+        for (Fish startFish : this.fishes) {
+            ArrayList<Fish> currentFishList = new ArrayList<>();
+            currentFishList.add(startFish);
+            System.out.println(startFish.toString());
+
+            ArrayList<ArrayList<Fish>> partlyResult = new ArrayList<>();
+            partlyResult = addFish(currentFishList, this.fishes, price - startFish.getPrice(), partlyResult);
+
+            for (ArrayList<Fish> actualResult : partlyResult){
+                System.out.println(actualResult.toString());
+            }
+            System.out.println();
+        }
     }
 
-    public void generateOptimalList(int price){
-//        for (Fish fish : this.fishes){
-//            ArrayList<Fish> partlyResult = new ArrayList<>();
-//            partlyResult.add(fish);
-//            addFish(partlyResult, price - fish.getPrice());
-//        }
-        ArrayList<Fish> partlyResult = new ArrayList<>();
-        partlyResult.add(this.fishes.get(0));
-        addFish(partlyResult, price - this.fishes.get(0).getPrice());
-    }
+    /**
+     * Recursive algorithm to generate a list of fish lists, which contain only fishes, who are
+     * tolerant to each other
+     * @param fishList  actual list of fishes, on which the algorithm work
+     * @param remainingFish actual list of fishes, on which new fishes were searched
+     * @param money remaining money
+     * @param completeList  complete List of fish list
+     * @return  A list of fish list, for which every fish is tolerant to each other
+     */
+    public ArrayList<ArrayList<Fish>> addFish(ArrayList<Fish> fishList, ArrayList<Fish> remainingFish,
+                                              int money, ArrayList<ArrayList<Fish>> completeList) {
 
-    private void addFish(ArrayList<Fish> fishList, int money) {
-        // create a new list of fish that can be added
-        // BC List as value are updated outside
-        ArrayList<Fish> remainFish = new ArrayList<>();
-        for (Fish potentialFish : this.fishes){
-            for (Fish actualFish : fishList){
-                if (!potentialFish.getName().equals(actualFish.getName()) &&
-                        checkTolerance(potentialFish, actualFish)){
-                    remainFish.add(potentialFish);
-                }
+        // create a new list of fish that can be added, check
+        // tolerance and the name (not added if same name)
+        Fish lastAddedFish = fishList.get(fishList.size() - 1);
+        ArrayList<Fish> updatedRemainingFishes = new ArrayList<>();
+        for (Fish potentialAddFish : remainingFish){
+            if (checkTolerance(lastAddedFish, potentialAddFish) &&
+                    !lastAddedFish.getName().equals(potentialAddFish.getName())){
+                updatedRemainingFishes.add(potentialAddFish);
             }
         }
 
-        // break condition: empty remaining fishes or money equals 0
-        if (remainFish.isEmpty() || money == 0) {
-            this.recursiveFishList.add(fishList);
-        }
-        // last fish was too much for our budget
-        if (money < 0) {
-            fishList.remove(fishList.size() - 1);
-            this.recursiveFishList.add(fishList);
+        // break condition 1 & 2: no more money or empty remaining fish list
+        if (money == 0 || updatedRemainingFishes.isEmpty()){
+            completeList.add(fishList);
+            return completeList;
         }
 
-        // create new list
-        for (Fish fish : remainFish){
-            ArrayList<Fish> tmp = new ArrayList<>(fishList);
-            tmp.add(fish);
-            addFish(tmp, money - fish.getPrice());
+        // there are fishes left, can only be added if enough remaining money
+        // check variable to check if any fish was added
+        boolean checkAdd = false;
+        for (Fish tolerantFish : updatedRemainingFishes){
+            if (money - tolerantFish.getPrice() >= 0) {
+                ArrayList<Fish> tmpFishlist = new ArrayList<>(fishList);
+                tmpFishlist.add(tolerantFish);
+                completeList = addFish(tmpFishlist, updatedRemainingFishes,
+                        money - tolerantFish.getPrice(), completeList);
+
+                checkAdd = true;
+            }
         }
 
+        // only add fish list if no other fish was added
+        if (!checkAdd){
+            completeList.add(fishList);
+        }
+        return completeList;
     }
 
+    /**
+     * Helper function to check if two fishes are tolerant to each other
+     * @param mainFish  One fish with a list of tolerated fishes
+     * @param addFish   The fish we want to add
+     * @return  Boolean if fish can be added
+     */
     private boolean checkTolerance (Fish mainFish, Fish addFish){
         // empty intolerance
         if (mainFish.getIntolerance().length == 0) return true;
